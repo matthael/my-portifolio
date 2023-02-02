80f70ded4882faee216c834631c57494968cde37
Biganao <mfponte2006@gmail.com>
2023-02-02 02:41:04 -0300

    fix: links to contact form
    add: new dependencies react-router-hash-link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,9 @@ import { BsArrowRightCircle } from "react-icons/bs";
 import BannerAstronaut from "../img/BannerAstronaut.svg";
 import styles from "./Banner.module.css";
 
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
@@ -60,43 +63,53 @@ export const Banner = () => {
   }, [isDeleting, loopNum, text.length]);
 
   return (
-    <section className={styles.banner} id="home">
-      <Container>
-        <Row className={styles.align_items_center}>
-          <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__fadeInLeft" : ""
-                  }
-                >
-                  <span className={styles.tagline}> Welcome to my Space </span>
-                  <h1>
-                    {`Hi, I'm `}
-                    <span className={styles.txt_rotate}>
-                      <span className={styles.wrap}>{text}</span>
+    <Router>
+      <section className={styles.banner} id="home">
+        <Container>
+          <Row className={styles.align_items_center}>
+            <Col xs={12} md={6} xl={7}>
+              <TrackVisibility>
+                {({ isVisible }) => (
+                  <div
+                    className={
+                      isVisible ? "animate__animated animate__fadeInLeft" : ""
+                    }
+                  >
+                    <span className={styles.tagline}>
+                      {" "}
+                      Welcome to my Space{" "}
                     </span>
-                  </h1>
-                  <p>
-                    lorem ipsum dolor sit amet consectetur adipiscing elit lorem
-                    ipsum dolor sit amet consectetur adipiscing elit lorem ipsum
-                    dolor sit amet consectetur adipiscing elit lorem ipsum dolor
-                    sit amet consectetur adipiscing elit lorem ipsum dolor sit
-                    amet consectetur adipiscing elit
-                  </p>
-                  <button>
-                    Let's Connect <BsArrowRightCircle size={25} />
-                  </button>
-                </div>
-              )}
-            </TrackVisibility>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <img src={BannerAstronaut} alt="Header Astronaut Img" />
-          </Col>
-        </Row>
-      </Container>
-    </section>
+                    <h1>
+                      {`Hi, I'm `}
+                      <span className={styles.txt_rotate}>
+                        <span className={styles.wrap}>{text}</span>
+                      </span>
+                    </h1>
+                    <p>
+                      lorem ipsum dolor sit amet consectetur adipiscing elit
+                      lorem ipsum dolor sit amet consectetur adipiscing elit
+                      lorem ipsum dolor sit amet consectetur adipiscing elit
+                      lorem ipsum dolor sit amet consectetur adipiscing elit
+                      lorem ipsum dolor sit amet consectetur adipiscing elit
+                    </p>
+                    <div className={styles.banner_Btn}>
+                      <HashLink to="#connect" activeClassName={styles.selected}>
+                        <button>
+                          <span> Let's Connect </span>
+                          <BsArrowRightCircle size={25} />
+                        </button>
+                      </HashLink>
+                    </div>
+                  </div>
+                )}
+              </TrackVisibility>
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+              <img src={BannerAstronaut} alt="Header Astronaut Img" />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Router>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,9 @@ import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 import { useState, useEffect } from "react";
 
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import Logo from "../img/Logo.svg";
 
 import styles from "./NavBar.module.css";
@@ -29,82 +32,86 @@ export const NavigationBar = () => {
   };
 
   return (
-    <Navbar
-      expand="lg"
-      id={styles.navbar}
-      className={scroll ? `${styles.scroll}` : ""}
-    >
-      <Container>
-        <Navbar.Brand href="#home">
-          <img src={Logo} alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" id={styles.nav_items}>
-            <Nav.Link
-              href="#home"
-              className={
-                activeLink === "home"
-                  ? `${styles.nav_link_item_active}`
-                  : `${styles.nav_link_item}`
-              }
-              onClick={() => onUpdateActiveLink("home")}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              href="#skills"
-              className={
-                activeLink === "skills"
-                  ? `${styles.nav_link_item_active}`
-                  : `${styles.nav_link_item}`
-              }
-              onClick={() => onUpdateActiveLink("skills")}
-            >
-              Skills
-            </Nav.Link>
-            <Nav.Link
-              href="#projects"
-              className={
-                activeLink === "projects"
-                  ? `${styles.nav_link_item_active}`
-                  : `${styles.nav_link_item}`
-              }
-              onClick={() => onUpdateActiveLink("projects")}
-            >
-              Projects
-            </Nav.Link>
-            <ul className={styles.social_icons}>
-              <li>
-                <a href="https://github.com/Biganao" target="blank">
-                  <FaGithub />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/matheuzinho_filipe/"
-                  target="blank"
-                >
-                  <FaInstagram />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/matheus-filipe-946430237/"
-                  target="blank"
-                >
-                  <FaLinkedin />
-                </a>
-              </li>
-            </ul>
-            <div className={styles.navbar_Btn}>
-              <button href="#connect">
-                <span> Let's Connect </span>
-              </button>
-            </div>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Router>
+      <Navbar
+        expand="lg"
+        id={styles.navbar}
+        className={scroll ? `${styles.scroll}` : ""}
+      >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" id={styles.nav_items}>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home"
+                    ? `${styles.nav_link_item_active}`
+                    : `${styles.nav_link_item}`
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills"
+                    ? `${styles.nav_link_item_active}`
+                    : `${styles.nav_link_item}`
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? `${styles.nav_link_item_active}`
+                    : `${styles.nav_link_item}`
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
+              <ul className={styles.social_icons}>
+                <li>
+                  <a href="https://github.com/Biganao" target="blank">
+                    <FaGithub />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/matheuzinho_filipe/"
+                    target="blank"
+                  >
+                    <FaInstagram />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/matheus-filipe-946430237/"
+                    target="blank"
+                  >
+                    <FaLinkedin />
+                  </a>
+                </li>
+              </ul>
+              <div className={styles.navbar_Btn}>
+                <HashLink to="#connect">
+                  <button>
+                    <span> Let's Connect </span>
+                  </button>
+                </HashLink>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
   );
 };
